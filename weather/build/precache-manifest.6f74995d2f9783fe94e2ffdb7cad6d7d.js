26dfd534e7a182f5100e71b858668dc025ec5517
art-kudashev <38160086+art-kudashev@users.noreply.github.com>
2019-03-29 20:51:31 +0200

    Add files via upload

---
+++ weather/build/precache-manifest.6f74995d2f9783fe94e2ffdb7cad6d7d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "23eaff6e8810509e24e6",
+    "url": "/static/js/main.87c679d1.chunk.js"
+  },
+  {
+    "revision": "bd2fad48c1340b2113c3",
+    "url": "/static/js/2.e08f731c.chunk.js"
+  },
+  {
+    "revision": "7f955e9e7df512d09b71a3de1043635c",
+    "url": "/index.html"
+  }
+];
