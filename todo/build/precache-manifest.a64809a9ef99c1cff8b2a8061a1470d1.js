3c0366a1dc2d64c91d0e90b313ca1c55f115ca62
art-kudashev <38160086+art-kudashev@users.noreply.github.com>
2019-03-29 20:25:07 +0200

    Add files via upload

---
+++ todo/build/precache-manifest.a64809a9ef99c1cff8b2a8061a1470d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7e33aba2d80b914b6369",
+    "url": "/static/js/main.17344d13.chunk.js"
+  },
+  {
+    "revision": "197d9ad31ce7bba7869a",
+    "url": "/static/js/2.0ed2724a.chunk.js"
+  },
+  {
+    "revision": "7e33aba2d80b914b6369",
+    "url": "/static/css/main.9ab4e445.chunk.css"
+  },
+  {
+    "revision": "027b1390b81922fab48750307d4fde71",
+    "url": "/index.html"
+  }
+];
