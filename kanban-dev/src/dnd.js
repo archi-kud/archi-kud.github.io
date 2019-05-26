037263159ebee59ba391ce4f6ea5e3961044d5d2
Artur Kudashev <art-kudashev@mail.ru>
2019-05-26 22:56:47 +0500

    kanban-dev

---
+++ kanban-dev/src/dnd.js
@@ -0,0 +1,190 @@
+const itemShadow = document.createElement('div');
+itemShadow.id = 'column-task-list__item-shadow';
+
+export default class DragManager {
+	constructor() {
+		this.dragItem = {};
+		this.shift = {};
+
+		this.dragItemClass = 'column-task-list__item';
+		this.dragAreaClass = 'column-task-list';
+
+		this.isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/.test(navigator.userAgent);
+	}
+
+	start = () => {
+		if (this.isMobile) {
+			window.addEventListener('touchstart', this.dragStart);
+			window.addEventListener('touchmove', this.drag);
+			window.addEventListener('touchend', this.dragEnd);
+		} else {
+			window.addEventListener('mousedown', this.dragStart);
+			window.addEventListener('mousemove', this.drag);
+			window.addEventListener('mouseup', this.dragEnd);
+		}
+	}
+
+	stop = () => {
+		if (this.isMobile) {
+			window.removeEventListener('touchstart', this.dragStart);
+			window.removeEventListener('touchmove', this.drag);
+			window.removeEventListener('touchend', this.dragEnd);
+		} else {
+			window.removeEventListener('mousedown', this.dragStart);
+			window.removeEventListener('mousemove', this.drag);
+			window.removeEventListener('mouseup', this.dragEnd);
+		}
+	}
+
+	dragStart = event => {
+		if (this.isMobile) {
+			if (event.which !== 0 || event.touches.length > 1 || !event.target.closest(`.${this.dragItemClass}`)) return;
+		} else {
+			if (event.which !== 1 || !event.target.closest(`.${this.dragItemClass}`)) return;
+		}
+		
+		const dragItem = this.dragItem;
+
+		dragItem.item = event.target.closest(`.${this.dragItemClass}`);
+
+		dragItem.item.style.width = dragItem.item.getBoundingClientRect().width + 'px';
+		dragItem.item.style.height = dragItem.item.getBoundingClientRect().height + 'px';
+
+		if (this.isMobile) {
+			this.shift = {x: dragItem.item.getBoundingClientRect().left - event.targetTouches[0].clientX, 
+					      y: dragItem.item.getBoundingClientRect().top - event.targetTouches[0].clientY};
+		} else {
+			this.shift = {x: dragItem.item.getBoundingClientRect().left - event.clientX, 
+				     	  y: dragItem.item.getBoundingClientRect().top - event.clientY};
+		}
+	}
+
+	drag = event => {
+		if (!this.dragItem.item) return;
+
+		const item = this.dragItem.item;
+
+		const dragAreaClass = this.dragAreaClass;
+		const dragItemClass = this.dragItemClass;
+
+		const dragArea = this.findElement(dragAreaClass);
+		const dragItem = this.findElement(dragItemClass);
+
+		const shift = this.shift;
+		const eventCoords = {x: this.isMobile ? event.targetTouches[0].clientX : event.clientX,
+							 y: this.isMobile ? event.targetTouches[0].clientY : event.clientY};
+
+		item.style.position = 'fixed';
+		item.style.top = eventCoords.y + shift.y + 'px';
+		item.style.left = eventCoords.x + shift.x + 'px';
+		item.style.transform = 'rotate(5deg)';
+		item.style.pointerEvents = 'none';
+		item.style.zIndex = 2;
+
+		if (this.dragItem.area) {
+			for (let child of this.dragItem.area.children) {
+				if (child.id === itemShadow.id && this.collideItems(item, child) === false) {
+					child.remove();
+				}
+			}
+		}
+	  	
+		if (dragArea) {
+			const itemArea = dragArea;
+
+			if (itemArea.children.length <= 1) {
+				itemArea.appendChild(itemShadow);
+				itemShadow.style.height = item.style.height;
+
+				this.dragItem.shadow = itemArea.children.namedItem(itemShadow.id);
+				this.dragItem.area = itemArea;
+			} 
+		}
+
+		if (dragItem) {
+			const collisionItem = dragItem;
+			const collisionItemList = collisionItem.parentElement;
+
+			const prevElement = collisionItem.previousElementSibling;
+			const nextElement = collisionItem.nextElementSibling;
+
+			if (this.getCollideDirection(collisionItem) === 'top' && this.collideItems(item, collisionItem)) {
+				if (prevElement && prevElement.id === itemShadow.id) return;
+				if (nextElement && nextElement.id === itemShadow.id) nextElement.remove();
+
+				collisionItemList.insertBefore(itemShadow, collisionItem);
+			}
+
+			if (this.getCollideDirection(collisionItem) === 'bottom' && this.collideItems(item, collisionItem)) {
+				if (nextElement && nextElement.id === itemShadow.id) return;
+				if (prevElement && prevElement.id === itemShadow.id) prevElement.remove();
+
+				this.insertAfter(itemShadow, collisionItem);
+			}
+
+			itemShadow.style.height = item.style.height;
+
+			this.dragItem.shadow = collisionItemList.children.namedItem(itemShadow.id);
+			this.dragItem.area = collisionItemList;
+			this.dragItem.collisionItem = collisionItem;
+		}
+	}
+
+	dragEnd = event => {
+		if (!this.dragItem.item) return;
+
+		const item = this.dragItem.item;
+
+		if (itemShadow && itemShadow.parentElement) {
+			item.setAttribute('style', 'none');
+			itemShadow.parentElement.replaceChild(item, itemShadow);
+		} else {
+			item.setAttribute('style', 'none');
+		}
+
+		this.dragItem.item = null;
+		this.dragItem.area = null;
+		this.dragItem.shadow = null;
+		this.dragItem.collisionItem = null;
+	}
+
+	findElement = (...attributeValue) => {
+		const event = window.event;
+		const eventCoords = {x: this.isMobile ? event.targetTouches[0].clientX : event.clientX,
+							 y: this.isMobile ? event.targetTouches[0].clientY : event.clientY};
+		let element = document.elementFromPoint(eventCoords.x, eventCoords.y);
+
+		for (let value of attributeValue) {
+			for (; element && element !== document; element = element.parentElement) {
+				if (element.classList.contains(value) || element.id === value) return element
+			}
+		}
+	}
+
+	getCollideDirection = item => {
+		const itemCoords = item.getBoundingClientRect();
+		const event = window.event;
+		const eventCoords = {x: this.isMobile ? event.targetTouches[0].clientX : event.clientX,
+							 y: this.isMobile ? event.targetTouches[0].clientY : event.clientY};
+
+		if ((eventCoords.y > itemCoords.top) && 
+			(eventCoords.y < itemCoords.top + itemCoords.height / 2)) return 'top';
+
+		if ((eventCoords.y < itemCoords.bottom) && 
+			(eventCoords.y > itemCoords.top + itemCoords.height / 2)) return 'bottom';
+	}
+
+	collideItems = (item1, item2) => {
+		const item1Coords = item1.getBoundingClientRect();
+		const item2Coords = item2.getBoundingClientRect();
+
+		return !((item1Coords.top + item1Coords.height < item2Coords.top)	   ||
+	     		 (item1Coords.top > item2Coords.top + item2Coords.height)      ||
+	     		 (item1Coords.left + item1Coords.width / 2 < item2Coords.left) ||
+	     		 (item1Coords.left + item1Coords.width / 2 > item2Coords.right));
+	}
+
+	insertAfter = (element, refElement) => {
+		return refElement.parentElement.insertBefore(element, refElement.nextElementSibling);
+	}
+}
---
+++ kanban-dev/src/index.js
@@ -0,0 +1,145 @@
+import React, { Component, Fragment } from 'react';
+import ReactDOM from 'react-dom';
+
+import DragManager from './components/DragManager';
+import BackgroundImageBlock from './components/BackgroundImageBlock';
+import BackgroundUploader from './components/BackgroundUploader';
+
+import './styles/main.css';
+
+class Board extends Component {
+
+	backgroundImageBlockRef = React.createRef();
+
+	state = {
+		columns: JSON.parse(localStorage.getItem('columns')),
+		backgroundImageURL: localStorage.getItem('backgroundImageURL')
+	};
+
+	addNewColumn = (title, columnId) => {
+		const columns = Object.assign([], JSON.parse(localStorage.getItem('columns')));
+		const newColumn = { title: title, tasks: [] };
+		
+		columns.splice(columnId, 1, newColumn);
+		columns.push({});
+
+		localStorage.setItem('columns', JSON.stringify(columns));
+		this.setState({ columns: columns });
+	};
+
+	addColumnTask = (newTask, columnId) => {
+		const columns = Object.assign([], JSON.parse(localStorage.getItem('columns')));
+		columns[columnId].tasks.push({ text: newTask });
+
+		localStorage.setItem('columns', JSON.stringify(columns));
+		this.setState({ columns: columns });
+	};
+
+	updateColumnTitle = (newTitle, columnId) => {
+		const columns = Object.assign([], JSON.parse(localStorage.getItem('columns')));
+
+		if (columns[columnId] && columns[columnId].title) {
+			columns[columnId].title = newTitle;
+
+			localStorage.setItem('columns', JSON.stringify(columns));
+			this.setState({ columns: columns });
+		}
+	};
+
+	updateColumnTasks = (tasks, columnId) => {
+		const columns = Object.assign([], JSON.parse(localStorage.getItem('columns')));
+
+		if (columns[columnId]) {
+			columns[columnId].tasks = tasks;
+
+			localStorage.setItem('columns', JSON.stringify(columns));
+			this.setState({ columns: columns });
+		}
+	};
+
+	updateColumnTask = (newTask, columnId, taskId) => {
+		const columns = Object.assign([], JSON.parse(localStorage.getItem('columns')));
+		
+		if (columns[columnId] && columns.length > 1 && columns[columnId].tasks[taskId]) {
+			columns[columnId].tasks[taskId].text = newTask;
+
+			localStorage.setItem('columns', JSON.stringify(columns));
+			this.setState({ columns: columns });
+		}
+	};
+
+	deleteColumnTask = (columnId, taskId) => {
+		const columns = Object.assign([], JSON.parse(localStorage.getItem('columns')));
+		columns[columnId].tasks.splice(taskId, 1);
+
+		localStorage.setItem('columns', JSON.stringify(columns));
+		this.setState({ columns: columns });
+	};
+
+	deleteColumn = columnId => {
+		const columns = Object.assign([], JSON.parse(localStorage.getItem('columns')));
+		columns.splice(columnId, 1);
+
+		localStorage.setItem('columns', JSON.stringify(columns));
+		this.setState({ columns: columns });
+	};
+
+	addBackgroundImage = event => {
+		const image = event.target.files[0];
+		const reader = new FileReader();
+		const imageBlock = this.backgroundImageBlockRef.current;
+
+		if (image.type.startsWith('image/')) {
+			reader.onload = () => {
+				imageBlock.style.background = `url(${reader.result})`;
+				imageBlock.style.backgroundSize = 'cover';
+
+				localStorage.setItem('backgroundImageURL', JSON.stringify(reader.result));
+				this.setState({ backgroundImageURL: reader.result });
+			};
+
+			reader.readAsDataURL(image);
+		}
+	};
+
+	deleteBackgroundImage = () => {
+		const imageBlock = this.backgroundImageBlockRef.current;
+		imageBlock.style.background = 'none';
+		
+		localStorage.setItem('backgroundImageURL', '');	
+		this.setState({ backgroundImageURL: '' });
+	};
+
+	componentDidMount() {
+		const imageBlock = this.backgroundImageBlockRef.current;
+		const imageURL = localStorage.getItem('backgroundImageURL');
+
+		if (imageURL) {
+			imageBlock.style.background = `url(${imageURL})`;
+			imageBlock.style.backgroundSize = 'cover';
+		}
+	}
+
+	render() {
+		const { columns } = this.state;
+		
+		return (
+			<Fragment>
+				<DragManager columns = { columns }
+							 addNewColumn = { this.addNewColumn } 
+							 addColumnTask = { this.addColumnTask }
+							 updateColumnTasks = { this.updateColumnTasks }
+							 updateColumnTitle = { this.updateColumnTitle }
+							 updateColumnTask = { this.updateColumnTask } 
+							 deleteColumnTask = { this.deleteColumnTask } 
+							 deleteColumn = { this.deleteColumn } />
+				<BackgroundImageBlock backgroundImageBlockRef = { this.backgroundImageBlockRef } />
+				<BackgroundUploader addBackgroundImage = { this.addBackgroundImage } 
+									deleteBackgroundImage = { this.deleteBackgroundImage }
+									backgroundImageBlockRef = { this.backgroundImageBlockRef } />
+			</Fragment>
+		)
+	}
+}
+
+ReactDOM.render(<Board />, document.getElementById('main-wrap'));
