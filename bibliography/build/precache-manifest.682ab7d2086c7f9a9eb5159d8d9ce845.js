051517b196c0e29c6c62bcce94073543ccdf863b
artur_kud <art-kudashev@mail.ru>
2019-08-27 10:27:11 +0300

    add bibliography

---
+++ bibliography/build/precache-manifest.682ab7d2086c7f9a9eb5159d8d9ce845.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c50c1e5ce2e192f1dc97eb64aae6b6",
+    "url": "/bibliography/build/index.html"
+  },
+  {
+    "revision": "5ff3bb2a65965c8dff04",
+    "url": "/bibliography/build/static/css/main.cf334a97.chunk.css"
+  },
+  {
+    "revision": "8108937517825c48a315",
+    "url": "/bibliography/build/static/js/2.cb882426.chunk.js"
+  },
+  {
+    "revision": "5ff3bb2a65965c8dff04",
+    "url": "/bibliography/build/static/js/main.aca588e2.chunk.js"
+  },
+  {
+    "revision": "6c64f0fddec3baec84a8",
+    "url": "/bibliography/build/static/js/runtime~main.367ec512.js"
+  },
+  {
+    "revision": "7c705bf82ec224bb8477351b548bc642",
+    "url": "/bibliography/build/static/media/PT-Root-UI-Regular.7c705bf8.ttf"
+  },
+  {
+    "revision": "99c150c79c3d4a021256a9446c7b2937",
+    "url": "/bibliography/build/static/media/PT-Root-UI-Regular.99c150c7.woff2"
+  },
+  {
+    "revision": "a16ca331f54e9d31c6a716438290f653",
+    "url": "/bibliography/build/static/media/PT-Root-UI-Regular.a16ca331.woff"
+  },
+  {
+    "revision": "f7552d29847c3c960359918481d7db43",
+    "url": "/bibliography/build/static/media/PT-Root-UI-Regular.f7552d29.eot"
+  },
+  {
+    "revision": "97260bdd4a58093f087e4bc9a79a2bb3",
+    "url": "/bibliography/build/static/media/books-on-the-ground.97260bdd.png"
+  },
+  {
+    "revision": "cf1abd24dd0360556d2cce722fc3b215",
+    "url": "/bibliography/build/static/media/copy.cf1abd24.svg"
+  },
+  {
+    "revision": "8b18a0bf7df071f11061da49912c578e",
+    "url": "/bibliography/build/static/media/cross-tiny.8b18a0bf.svg"
+  },
+  {
+    "revision": "12018789027b49f3ba4d7370ed7f9783",
+    "url": "/bibliography/build/static/media/cross.12018789.svg"
+  },
+  {
+    "revision": "ae4efca127e2e3c35cd701380be4e8bb",
+    "url": "/bibliography/build/static/media/gold-carrot-logo.ae4efca1.svg"
+  },
+  {
+    "revision": "3879a4b2b12cfdc96d3a11df2bd10ee5",
+    "url": "/bibliography/build/static/media/link.3879a4b2.svg"
+  },
+  {
+    "revision": "8fe8c2f01e573bc3ff6cdd83e96150b8",
+    "url": "/bibliography/build/static/media/logo.8fe8c2f0.svg"
+  },
+  {
+    "revision": "f1e02cd60e94b757bb0d3285ba34356f",
+    "url": "/bibliography/build/static/media/plus.f1e02cd6.svg"
+  },
+  {
+    "revision": "47fc50ecfc81f8b44c731e7bcc3d0757",
+    "url": "/bibliography/build/static/media/search.47fc50ec.svg"
+  }
+]);
