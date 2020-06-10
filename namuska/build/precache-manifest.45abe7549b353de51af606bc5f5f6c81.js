a8aaecb12ec4cdc02a0af598d81b036e370a388a
Artur Kudashev <art-kudashev@mail.ru>
2020-06-10 13:29:21 +0300

    Add namuska

---
+++ namuska/build/precache-manifest.45abe7549b353de51af606bc5f5f6c81.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195c79c567ecd3aabeeb7b41ee847e1e",
+    "url": "/namuska/build/index.html"
+  },
+  {
+    "revision": "5011a50e0cf63cd07d94",
+    "url": "/namuska/build/static/css/main.80c33986.chunk.css"
+  },
+  {
+    "revision": "3c119ddb33f180e95c86",
+    "url": "/namuska/build/static/js/2.8560b636.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/namuska/build/static/js/2.8560b636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5011a50e0cf63cd07d94",
+    "url": "/namuska/build/static/js/main.d7d25d69.chunk.js"
+  },
+  {
+    "revision": "e193d13199729015dfc0",
+    "url": "/namuska/build/static/js/runtime-main.ad12626c.js"
+  },
+  {
+    "revision": "1d7cb3538ba2ff056172f66fcf8e51d5",
+    "url": "/namuska/build/static/media/Exo-Thin.1d7cb353.eot"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/namuska/build/static/media/Exo-Thin.44bde874.otf"
+  },
+  {
+    "revision": "44fbebfce5c95819cb765809cfe24b71",
+    "url": "/namuska/build/static/media/Exo-Thin.44fbebfc.woff"
+  },
+  {
+    "revision": "b8fd437dca4b38408c905606dd534024",
+    "url": "/namuska/build/static/media/Exo-Thin.b8fd437d.ttf"
+  },
+  {
+    "revision": "7afeaf2b8e5fee8b3332318a9f216092",
+    "url": "/namuska/build/static/media/TrebuchetMS-Bold.7afeaf2b.ttf"
+  },
+  {
+    "revision": "9b45a5e6fd7e43502d74c1cf2bb77ded",
+    "url": "/namuska/build/static/media/TrebuchetMS-Bold.9b45a5e6.woff"
+  },
+  {
+    "revision": "ca7ac163de240c9e7d6fc018ad0f4dad",
+    "url": "/namuska/build/static/media/TrebuchetMS-Bold.ca7ac163.eot"
+  },
+  {
+    "revision": "34723a2fbef5882977e8392d020956af",
+    "url": "/namuska/build/static/media/TrebuchetMS.34723a2f.woff"
+  },
+  {
+    "revision": "530d3166bc7f3ee970eec68df2a2bd2e",
+    "url": "/namuska/build/static/media/TrebuchetMS.530d3166.ttf"
+  },
+  {
+    "revision": "be77a7db37f37471cff8616dc5b875c5",
+    "url": "/namuska/build/static/media/TrebuchetMS.be77a7db.eot"
+  },
+  {
+    "revision": "90a57c97657ad433029f211f7886300b",
+    "url": "/namuska/build/static/media/background.90a57c97.jpg"
+  },
+  {
+    "revision": "3459754a60a816a7a2b538193f04299c",
+    "url": "/namuska/build/static/media/cat.3459754a.png"
+  },
+  {
+    "revision": "44bccc9a4a4f3a6e81b7e308a6ce2024",
+    "url": "/namuska/build/static/media/product-background.44bccc9a.svg"
+  }
+]);
